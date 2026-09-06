849df566e7bb5926aeec9fe6d98ef43ed7272dfb
Tim Prince <tim.prince@gmail.com>
2026-09-05 19:04:37 -0700

    Use determinate-nix after the switch, too.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
   description = "NixOS configuration for on-demand AWS GitHub Actions runners";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, determinate }: {
     nixosConfigurations.gh-runner = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
         "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
+        determinate.nixosModules.default
         ./gh-runner.nix
       ];
     };
